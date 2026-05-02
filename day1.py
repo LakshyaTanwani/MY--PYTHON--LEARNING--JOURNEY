506520e3e7457225bbc0e7fd2986232d926ffbb4
LakshyaTanwani <lakshyatanwani698@gmail.com>
2026-05-02 16:48:16 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+#ask user for their name 
+name=input("whats your name? ")
+#say hello to user
+#end helps us to complete sentence and move cursor to next line
+print("hello,", end="")
+print(name)
+#to use quotes within quotes we need to use/word/ to prevent syntax error
